bed7f869016292d69676b1c64a15bb7031de1509
dumitrescustefan <dumitrescu.stefan@gmail.com>
2019-05-08 08:59:09 +0300

    readded lstm_att (by Andrei)

---
+++ models/lstm_attn/model.py
@@ -0,0 +1,73 @@
+import torch.nn as nn
+import torch
+
+
+class Encoder(nn.Module):
+    def __init__(self, n_class, n_emb_dim, n_hidden, n_lstm_units, n_lstm_dropout):
+        super(Encoder, self).__init__()
+        self.embedding_layer = nn.Embedding(n_class, n_emb_dim)
+        self.lstm = nn.LSTM(n_emb_dim, n_hidden, n_lstm_units, dropout=n_lstm_dropout,
+                            bidirectional=True, batch_first=True)
+
+    def forward(self, input):
+        embeddings = self.embedding_layer(input)
+
+        output, states = self.lstm(embeddings)
+
+        return output, states
+
+
+class AttnDecoder(nn.Module):
+    def __init__(self, n_class, n_emb_dim, n_hidden, n_lstm_units, n_lstm_dropout, n_dropout):
+        super(AttnDecoder, self).__init__()
+        self.embedding_layer = nn.Embedding(n_class, n_emb_dim)
+        self.attn1 = nn.Linear(n_hidden*n_lstm_units * 2 + n_hidden * 2, n_hidden * 4)
+        self.attn2 = nn.Linear(n_hidden * 4, 1)
+        self.dropout = nn.Dropout(n_dropout)
+        self.lstm = nn.LSTM(n_emb_dim + n_hidden * 2, n_hidden, n_lstm_units, dropout=n_lstm_dropout,
+                            bidirectional=True, batch_first=True)
+        self.softmax = nn.Softmax(dim=1)
+        self.output_layer = nn.Linear(n_hidden * 2, n_class)
+
+    def _calculate_context_vector(self, state_h, enc_output):
+        state_h = state_h.permute(1, 0, 2).reshape(enc_output.shape[0], 1, -1).expand(-1, enc_output.shape[1], -1)
+
+        attn_input = torch.cat((enc_output, state_h), dim=2)
+        attn_hidden = self.attn1(attn_input)
+        attn_hidden = self.dropout(attn_hidden)
+        attn_output = self.attn2(attn_hidden)
+        attn_weights = self.softmax(attn_output)
+
+        context_vector = torch.mul(enc_output, attn_weights)
+        return torch.sum(context_vector, 1)
+
+    def forward(self, input, enc_output, enc_states):
+        embeddings = self.embedding_layer(input)
+
+        context_vector = self._calculate_context_vector(enc_states[0], enc_output)
+        lstm_input = torch.cat((embeddings[:, 0, :], context_vector), dim=1).reshape(enc_output.shape[0], 1, -1)
+        dec_output, dec_states = self.lstm(lstm_input, enc_states)
+
+        for i in range(1, input.shape[1]):
+            context_vector = self._calculate_context_vector(dec_states[0], enc_output)
+            lstm_input = torch.cat((embeddings[:, i, :], context_vector), dim=1).reshape(enc_output.shape[0], 1, -1)
+            curr_dec_output, dec_states = self.lstm(lstm_input, dec_states)
+            dec_output = torch.cat((dec_output, curr_dec_output), dim=1)
+
+        output = self.output_layer(dec_output)
+
+        return output
+
+
+class LSTMAttnEncoderDecoder(nn.Module):
+    def __init__(self, n_class, n_emb_dim, n_hidden, n_lstm_units, n_lstm_dropout, n_dropout):
+        super(LSTMAttnEncoderDecoder, self).__init__()
+        self.encoder = Encoder(n_class, n_emb_dim, n_hidden, n_lstm_units, n_lstm_dropout)
+        self.decoder = AttnDecoder(n_class, n_emb_dim, n_hidden, n_lstm_units, n_lstm_dropout, n_dropout)
+
+    def forward(self, x, y):
+        enc_output, enc_states = self.encoder.forward(x)
+        # print(enc_states)
+        output = self.decoder.forward(y, enc_output, enc_states)
+
+        return output
---
+++ models/lstm_attn/train.py
@@ -0,0 +1,105 @@
+# add package root
+import os, sys
+sys.path.insert(0, '../..')
+
+
+from data.roen.loader import loader
+
+from models.lstm_attn.model import LSTMAttnEncoderDecoder
+
+
+if __name__ == "__main__":
+    data_folder = os.path.join("..", "..", "data", "roen", "ready", "setimes.8K.bpe")
+
+    batch_size = 15
+    min_seq_len = 5
+    max_seq_len = 10000
+
+    print("Loading data ...")
+    train_loader, valid_loader, test_loader, src_w2i, src_i2w, tgt_w2i, tgt_i2w = loader(data_folder, batch_size, max_seq_len, min_seq_len)
+    
+    print("Loading done, train instances {}, dev instances {}, test instances {}, vocab size {}\n".format(
+        len(train_loader.dataset.X),
+        len(valid_loader.dataset.X),
+        len(test_loader.dataset.X),
+        len(src_w2i)))
+
+    train_loader.dataset.X = train_loader.dataset.X[0:300]
+    train_loader.dataset.y = train_loader.dataset.y[0:300]
+    valid_loader.dataset.X = valid_loader.dataset.X[0:300]
+    valid_loader.dataset.y = valid_loader.dataset.y[0:300]
+
+    n_class = len(src_w2i)
+    n_emb_dim = 300
+    n_hidden = 128
+    n_lstm_units = 2
+    n_lstm_dropout = 0.2
+    n_dropout = 0.3
+
+    model = LSTMAttnEncoderDecoder(n_class, n_emb_dim, n_hidden, n_lstm_units, n_lstm_dropout, n_dropout)
+
+    epochs = 500
+    lr = 0.1
+    train(model, epochs, lr, batch_size, n_class, train_loader, valid_loader, tgt_i2w)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    # embedding_dim = 512
+    # encoder_hidden_dim = 512
+    # decoder_hidden_dim = 512
+    # encoder_n_layers = 2
+    # decoder_n_layers = 1
+    # encoder_drop_prob = 0.3
+    # decoder_drop_prob = 0.3
+    # lr = 0.0005
+    #
+    # model = LSTMEncoderDecoderAtt(src_w2i, src_i2w, tgt_w2i, tgt_i2w, embedding_dim, encoder_hidden_dim,
+    #                             decoder_hidden_dim, encoder_n_layers, decoder_n_layers,
+    #                             encoder_drop_prob=encoder_drop_prob, decoder_drop_prob=decoder_drop_prob, lr=lr,
+    #                             model_store_path="../../train/lstm_att")
+    #
+    # model.train(train_loader, valid_loader, test_loader, batch_size, patience=20)
+
+
+# run
+#net.load_checkpoint("best")
+#input = [ [4,5,6,7,8,9], [9,8,7,6] ]
+#output = net.run(input)
+#output = net.run(valid_loader, batch_size)
+#print(output)
